b09ee9887c9c0e3932b5fa8e8590cfd9d6e4864c
nasibullo03 <nasibullo.khalilov@bk.ru>
2022-09-08 13:27:23 +0700

    Оптимизирован код

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -10,31 +12,21 @@ namespace Programing_Labs
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
-            InitializeComponent();
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-           /* var converter = new System.Windows.Media.BrushConverter();
-            Rectangle rect = new Rectangle(){
-                Width = 10, Height=20,
-                StrokeThickness = 1,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Fill = (Brush)converter.ConvertFromString("#48465e"),
-                Visibility = Visibility.Visible,
-        };
-            
-            MenuItemList.Children.Add(rect);
-
            
-            *//*< Rectangle x: Name = "MenuItemRectangle"
-                                       Width = "10"
-                                       StrokeThickness = "1"
-                                       Fill = "#48465e"
-                                       HorizontalAlignment = "Left"
-                            />*/
+            InitializeComponent();
+            
+
+        }
+        
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            new Design.MenuDesign().AddMenuItems();
+
         }
     }
 }
