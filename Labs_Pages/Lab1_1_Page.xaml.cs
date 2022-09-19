3dac30262db6dacf8310e2a2437976ed3697dc93
nasibullo03 <nasibullo.khalilov@bk.ru>
2022-09-19 14:58:54 +0700

    Добавлен новая страница первой лабы. Оптимизирован код.

---
+++ Labs_Pages/Lab1_1_Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Programing_Labs.Labs_Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Lab1_1_Page.xaml
+    /// </summary>
+    public partial class Lab1_1_Page : Page
+    {
+        public Lab1_1_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
