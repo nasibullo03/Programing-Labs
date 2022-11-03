b7c8a26b3cf1750f42b32bef28abd3c1afe4b566
nasibullo03 <nasibullo.khalilov@bk.ru>
2022-11-03 16:43:08 +0700

    Добавил класс для решение квадратичного функция. В классе  Excel изменил некоторые значение и добавил Class LinearFunction и QuadraticalFunction внутри класса Excel и LeastSquareMethod. Сделал перегрузка методов для метода ShowGraph. Активировал WpfPlot.Legend().

--- LabsClases/Excell.cs
+++ LabsClases/Excell.cs
@@ -19,7 +19,10 @@ namespace Programing_Labs.LabsClases
         private static object missValue = System.Reflection.Missing.Value;
 
         private static string[] linearTemplateValues = new string[] {
-            "x","y","x^2","x*y","y лин","d","d^2","Сумма","x^2","=A1","","=D1"
+            "x","y","x^2","x*y","y лин","d","d^2"
+        }; 
+        private static string[] QuadraticalTemplateValues = new string[] {
+            "x","y","x^2","x*y","X^3","x^4","x^2*y","y кв","d","d^2"
         };
         private static int LastPointIndex { get; set; }
 
@@ -28,9 +31,8 @@ namespace Programing_Labs.LabsClases
             XlApplication = new Microsoft.Office.Interop.Excel.Application();
             XlWorkbook = XlApplication.Workbooks.Add(missValue);
             XlWorksheet = (Worksheet)XlWorkbook.Worksheets.Item[1];
-            
-        }
 
+        }
         public static void SaveFile()
         {
             XlWorkbook.SaveAs("d:\\csharp-Excel.xls", XlFileFormat.xlWorkbookNormal, missValue, missValue, missValue, missValue, XlSaveAsAccessMode.xlExclusive, missValue, missValue, missValue, missValue, missValue);
@@ -46,82 +48,197 @@ namespace Programing_Labs.LabsClases
             Marshal.ReleaseComObject(XlWorkbook);
             Marshal.ReleaseComObject(XlApplication);
         }
-        public static void FillLinearTemplate()
-        {
 
-            for (int i = 1; i <= linearTemplateValues.Length; ++i)
+        public class LinearFunction
+        {
+            public static void FillTemplate()
             {
-                XlWorksheet.Cells[1, i] = linearTemplateValues[i - 1];
+
+                for (int i = 1; i <= linearTemplateValues.Length; ++i)
+                {
+                    XlWorksheet.Cells[1, i] = linearTemplateValues[i - 1];
+                }
+
             }
-
-        }
-        public static void AddListViewsData(LeastSquareMethod.LinearFunction function)
-        {
-            int count = 2;
-            for(int i=0;i<function.XValue.Length;++i)
+            public static void AddListViewsData(LeastSquareMethod.LinearFunction function)
             {
-                //добавляем значение x и y в таблице
-                XlWorksheet.Cells[count, 1] = function.XValue[i];
-                XlWorksheet.Cells[count, 2] = function.YValue[i];
-                //рассчитаем значение x^2 и x*y
-                XlWorksheet.Cells[count, 3] = function.PowX2[i];
-                XlWorksheet.Cells[count, 4] = function.MultXY[i];
+                int count = 2;
+                for (int i = 0; i < function.XValue.Length; ++i)
+                {
+                    //добавляем значение x и y в таблице
+                    XlWorksheet.Cells[count, 1] = function.XValue[i];
+                    XlWorksheet.Cells[count, 2] = function.YValue[i];
+                    //рассчитаем значение x^2 и x*y
+                    XlWorksheet.Cells[count, 3] = function.PowX2[i];
+                    XlWorksheet.Cells[count, 4] = function.MultXY[i];
+                    ++count;
+                }
+
                 ++count;
+                //рассчитаем сумма рядов
+                XlWorksheet.Cells[count, 1] = LeastSquareMethod.Sum(function.XValue);
+                XlWorksheet.Cells[count, 2] = LeastSquareMethod.Sum(function.YValue);
+                XlWorksheet.Cells[count, 3] = LeastSquareMethod.Sum(function.PowX2);
+                XlWorksheet.Cells[count, 4] = LeastSquareMethod.Sum(function.MultXY);
+
+                //сохраняем последный индекс, чтобы в дальнейшем получить доступ к сумму элементов
+                LastPointIndex = count;
+
             }
 
-            ++count;
-            //рассчитаем сумма рядов
-            XlWorksheet.Cells[count, 1] = LeastSquareMethod.Sum(function.XValue);
-            XlWorksheet.Cells[count, 2] = LeastSquareMethod.Sum(function.YValue);
-            XlWorksheet.Cells[count, 3] = LeastSquareMethod.Sum(function.PowX2);
-            XlWorksheet.Cells[count, 4] = LeastSquareMethod.Sum(function.MultXY);
-
-            //сохраняем последный индекс, чтобы в дальнейшем получить доступ к сумму элементов
-            LastPointIndex = count;
-
-        }
-        
-        public static void FillMatrixsValues(LeastSquareMethod.LinearFunction function)
-        {
-            XlWorksheet.Cells[2, 9] = function.Matrix1.Value[0,0];
-            XlWorksheet.Cells[2, 10] = function.Matrix1.Value[0, 1];
-            
-
-            XlWorksheet.Cells[3, 9] = function.Matrix1.Value[1, 0]; 
-            XlWorksheet.Cells[3, 10] = function.Matrix1.Value[1, 1];
-
-            XlWorksheet.Cells[2, 12] = function.Matrix2.Value[0,0];
-            XlWorksheet.Cells[3, 12] = function.Matrix2.Value[1, 0];
-
-            XlWorksheet.Cells[5, 8] = "Обратная матрица:";
-            XlWorksheet.Cells[5, 9] = function.MatrixInverse.Value[0,0];
-            XlWorksheet.Cells[5, 10] = function.MatrixInverse.Value[0,1];
-            XlWorksheet.Cells[6, 9] = function.MatrixInverse.Value[1,0];
-            XlWorksheet.Cells[6, 10] = function.MatrixInverse.Value[1,1];
-            XlWorksheet.Cells[6, 10] = function.MatrixInverse.Value[1,1];
-            
-            
-            XlWorksheet.Cells[5, 11] = "a = ";
-            XlWorksheet.Cells[6, 11] = "b = ";
-            
-            XlWorksheet.Cells[5, 12] = function.MatrixResult.Value[0,0];
-            XlWorksheet.Cells[6, 12] = function.MatrixResult.Value[1, 0];
-
-            int count = 2;
-            for (int i=0; i < function.Ylinear.Length; ++i)
+            public static void FillMatrixsValues(LeastSquareMethod.LinearFunction function)
             {
-                XlWorksheet.Cells[count, 5] = function.Ylinear[i];
-                XlWorksheet.Cells[count, 6] = function.D[i];
-                XlWorksheet.Cells[count, 7] = function.PowD2[i];
-                ++count;
+                XlWorksheet.Cells[2, 9] = function.Matrix1.Value[0, 0];
+                XlWorksheet.Cells[2, 10] = function.Matrix1.Value[0, 1];
+
+
+                XlWorksheet.Cells[3, 9] = function.Matrix1.Value[1, 0];
+                XlWorksheet.Cells[3, 10] = function.Matrix1.Value[1, 1];
+
+                XlWorksheet.Cells[2, 12] = function.Matrix2.Value[0, 0];
+                XlWorksheet.Cells[3, 12] = function.Matrix2.Value[1, 0];
+
+                XlWorksheet.Cells[5, 8] = "Обратная матрица:";
+                XlWorksheet.Cells[5, 9] = function.MatrixInverse.Value[0, 0];
+                XlWorksheet.Cells[5, 10] = function.MatrixInverse.Value[0, 1];
+                XlWorksheet.Cells[6, 9] = function.MatrixInverse.Value[1, 0];
+                XlWorksheet.Cells[6, 10] = function.MatrixInverse.Value[1, 1];
+
+
+                XlWorksheet.Cells[5, 11] = "a = ";
+                XlWorksheet.Cells[6, 11] = "b = ";
+
+                XlWorksheet.Cells[5, 12] = function.MatrixResult.Value[0, 0];
+                XlWorksheet.Cells[6, 12] = function.MatrixResult.Value[1, 0];
+
+                int count = 2;
+                for (int i = 0; i < function.Ylinear.Length; ++i)
+                {
+                    XlWorksheet.Cells[count, 5] = function.Ylinear[i];
+                    XlWorksheet.Cells[count, 6] = function.D[i];
+                    XlWorksheet.Cells[count, 7] = function.PowD2[i];
+                    ++count;
+                }
+
+                XlWorksheet.Cells[LastPointIndex, 7] = "Невязка = ";
+                XlWorksheet.Cells[LastPointIndex, 8] = LeastSquareMethod.Sum(function.PowD2);
+
             }
-           
-            XlWorksheet.Cells[LastPointIndex, 7] = "Невязка = ";
-            XlWorksheet.Cells[LastPointIndex, 8] = LeastSquareMethod.Sum(function.PowD2);
-
-
-
         }
+        public class QuadraticFunction
+        {
+            public static void FillTemplate()
+            {
+
+                for (int i = 1; i <= QuadraticalTemplateValues.Length; ++i)
+                {
+                    XlWorksheet.Cells[1, i] = QuadraticalTemplateValues[i - 1];
+                }
+
+            }
+            public static void AddListViewsData(LeastSquareMethod.QuadraticFunction function)
+            {
+                int count = 2;
+                for (int i = 0; i < function.XValue.Length; ++i)
+                {
+                    //добавляем значение x и y в таблице
+                    XlWorksheet.Cells[count, 1] = function.XValue[i];
+                    XlWorksheet.Cells[count, 2] = function.YValue[i];
+                    //рассчитаем значение x^2 и x*y
+                    XlWorksheet.Cells[count, 3] = function.PowX2[i];
+                    XlWorksheet.Cells[count, 4] = function.MultXY[i];
+                    XlWorksheet.Cells[count, 5] = function.PowX3[i];
+                    XlWorksheet.Cells[count, 6] = function.PowX4[i];
+                    XlWorksheet.Cells[count, 7] = function.PowX2MultY[i];
+                    XlWorksheet.Cells[count, 8] = function.YQuadratical[i];
+                    XlWorksheet.Cells[count, 9] = function.D[i];
+                    XlWorksheet.Cells[count, 10] = function.PowD2[i];
+                    ++count;
+                }
+
+                ++count;
+                //рассчитаем сумма рядов
+                XlWorksheet.Cells[count, 1] = function.SumXValue;
+                XlWorksheet.Cells[count, 2] = function.SumYValue;
+                XlWorksheet.Cells[count, 3] = function.SumPowX2;
+                XlWorksheet.Cells[count, 4] = function.SumMultXY;
+                XlWorksheet.Cells[count, 5] = function.SumPowX3;
+                XlWorksheet.Cells[count, 6] = function.SumPowX4;
+                XlWorksheet.Cells[count, 7] = function.SumPowX2MultY;
+                XlWorksheet.Cells[count, 10] = function.S;
+
+                //сохраняем последный индекс, чтобы в дальнейшем получить доступ к сумму элементов
+                LastPointIndex = count;
+
+            }
+
+            public static void FillMatrixsValues(LeastSquareMethod.QuadraticFunction function)
+            {
+                LastPointIndex += 4;
+                XlWorksheet.Cells[LastPointIndex, 2] = function.Matrix1.Value[0, 0];
+                XlWorksheet.Cells[LastPointIndex, 3] = function.Matrix1.Value[0, 1];
+                XlWorksheet.Cells[LastPointIndex, 4] = function.Matrix1.Value[0, 2];
+
+                XlWorksheet.Cells[LastPointIndex+1, 2] = function.Matrix1.Value[1, 0];
+                XlWorksheet.Cells[LastPointIndex+1, 3] = function.Matrix1.Value[1, 1];
+                XlWorksheet.Cells[LastPointIndex+1, 4] = function.Matrix1.Value[1, 2];
+
+                XlWorksheet.Cells[LastPointIndex + 2, 2] = function.Matrix1.Value[2, 0];
+                XlWorksheet.Cells[LastPointIndex + 2, 3] = function.Matrix1.Value[2, 1];
+                XlWorksheet.Cells[LastPointIndex + 2, 4] = function.Matrix1.Value[2, 2];
+
+                XlWorksheet.Cells[LastPointIndex, 6] = function.Matrix2.Value[0, 0];
+                XlWorksheet.Cells[LastPointIndex+1, 6] = function.Matrix2.Value[1, 0];
+                XlWorksheet.Cells[LastPointIndex+2, 6] = function.Matrix2.Value[2, 0];
+
+                LastPointIndex += 4;
+                XlWorksheet.Cells[LastPointIndex-1, 2] = "Обратная матрица:";
+                XlWorksheet.Cells[LastPointIndex, 2] = function.MatrixInverse.Value[0, 0];
+                XlWorksheet.Cells[LastPointIndex, 3] = function.MatrixInverse.Value[0, 1];
+                XlWorksheet.Cells[LastPointIndex, 4] = function.MatrixInverse.Value[0, 2];
+
+                XlWorksheet.Cells[LastPointIndex+1, 2] = function.MatrixInverse.Value[1, 0];
+                XlWorksheet.Cells[LastPointIndex+1, 3] = function.MatrixInverse.Value[1, 1];
+                XlWorksheet.Cells[LastPointIndex+1, 4] = function.MatrixInverse.Value[1, 2];
+
+                XlWorksheet.Cells[LastPointIndex + 2, 2] = function.MatrixInverse.Value[2, 0];
+                XlWorksheet.Cells[LastPointIndex + 2, 3] = function.MatrixInverse.Value[2, 1];
+                XlWorksheet.Cells[LastPointIndex + 2, 4] = function.MatrixInverse.Value[2, 2];
+
+
+                XlWorksheet.Cells[LastPointIndex, 5] = "a = ";
+                XlWorksheet.Cells[LastPointIndex+1, 5] = "b = ";
+                XlWorksheet.Cells[LastPointIndex+2, 5] = "c = ";
+
+                XlWorksheet.Cells[LastPointIndex, 12] = function.MatrixResult.Value[0, 0];
+                XlWorksheet.Cells[LastPointIndex+1, 12] = function.MatrixResult.Value[1, 0];
+                XlWorksheet.Cells[LastPointIndex+2, 12] = function.MatrixResult.Value[2, 0];
+
+            }
+            public static void AddChart()
+            {
+                ChartObjects ChartObjects = (ChartObjects)XlWorksheet.ChartObjects();
+
+                ChartObject chartObject = ChartObjects.Add(40, 1000, 700, 350);
+
+                chartObject.Chart.ChartType = XlChartType.xlLineMarkers;
+                chartObject.Chart.HasTitle = true;
+                
+                Range xValRange = XlWorksheet.Range["A1", "A5"];
+
+                SeriesCollection oSeriesCollection = (SeriesCollection)chartObject.Chart.SeriesCollection();
+
+                for (int i = 0; i < oSeriesCollection.Count; i++)
+                {
+
+                    Series oSeries = oSeriesCollection.NewSeries();
+
+                    oSeries.Values = XlWorksheet.Range["B1", "B5"];
+
+                }
+            }
+        }
+
 
 
 
--- LabsClases/LeastSquareMethod.cs
+++ LabsClases/LeastSquareMethod.cs
@@ -8,7 +8,6 @@ namespace Programing_Labs.LabsClases
 {
     class LeastSquareMethod
     {
-
         public static double[] X { get; private set; }
         public static double[] Y { get; private set; }
         private static int Count { get; set; }
@@ -37,28 +36,29 @@ namespace Programing_Labs.LabsClases
             return sum; ;
         }
 
-       
+
         public class LinearFunction
         {
-            public double[]XValue { get; private set; }
-            public double[]YValue { get; private set; }
-            public  double[] PowX2 { get; private set; }
-            public  double[] MultXY { get; private set; }
-            public  double[] Ylinear { get; private set; }
-            public  double[] D { get; private set; }
-            public  double A { get; private set; }
-            public  double B { get; private set; }
-            public  double S { get; private set; }
-            public  double[] PowD2 { get; private  set; }
-            public  Matrix Matrix1 { get; private set; }
-            public  Matrix Matrix2 { get; private set; }
-            public  Matrix MatrixInverse { get; private set; }
-            public  Matrix MatrixResult { get; private set; }
-            
-            
-            public void FillBasicValues()
+            public LinearFunction()
             {
-                
+            }
+            public double[] XValue { get; private set; }
+            public double[] YValue { get; private set; }
+            public double[] PowX2 { get; private set; }
+            public double[] MultXY { get; private set; }
+            public double[] Ylinear { get; private set; }
+            public double[] D { get; private set; }
+            public double A { get; private set; }
+            public double B { get; private set; }
+            public double S { get; private set; }
+            public double[] PowD2 { get; private set; }
+            public Matrix Matrix1 { get; private set; }
+            public Matrix Matrix2 { get; private set; }
+            public Matrix MatrixInverse { get; private set; }
+            public Matrix MatrixResult { get; private set; }
+            public void Solve()
+            {
+
                 FillValues(GraphicPoint.GraphicPoints);
                 PowX2 = new double[Count];
                 MultXY = new double[Count];
@@ -96,15 +96,94 @@ namespace Programing_Labs.LabsClases
                 YValue = Y;
             }
 
-            
-
-
-
-
         }
         public class QuadraticFunction
         {
+            public double[] XValue { get; private set; }
+            public double[] YValue { get; private set; }
+            public double[] PowX2 { get; private set; }
+            public double[] PowX3 { get; private set; }
+            public double[] PowX4 { get; private set; }
+            public double[] PowX2MultY { get; private set; }
+            public double[] MultXY { get; private set; }
+            public double[] YQuadratical { get; private set; }
+            public double[] D { get; private set; }
+            public double A { get; private set; }
+            public double B { get; private set; }
+            public double C { get; private set; }
+            public double S { get; private set; }
+            public double[] PowD2 { get; private set; }
+            public double SumXValue { get; private set; }
+            public double SumYValue { get; private set; }
+            public double SumPowX2 { get; private set; }
+            public double SumPowX3 { get; private set; }
+            public double SumPowX4 { get; private set; }
+            public double SumPowX2MultY { get; private set; }
+            public double SumMultXY { get; private set; }
 
+            public Matrix Matrix1 { get; private set; }
+            public Matrix Matrix2 { get; private set; }
+            public Matrix MatrixInverse { get; private set; }
+            public Matrix MatrixResult { get; private set; }
+
+
+            public void Solve()
+            {
+
+                FillValues(GraphicPoint.GraphicPoints);
+                XValue = X;
+                YValue = Y;
+
+                PowX2 = new double[Count];
+                PowX3 = new double[Count];
+                PowX4 = new double[Count];
+                PowX2MultY = new double[Count];
+                MultXY = new double[Count];
+                YQuadratical = new double[Count];
+                D = new double[Count];
+                PowD2 = new double[Count];
+                //заполняем значение нижеуказанных рядов
+                for (int i = 0; i < Count; ++i)
+                {
+                    PowX2[i] = Math.Pow(XValue[i], 2);
+                    MultXY[i] = X[i] * YValue[i];
+                    PowX3[i] = Math.Pow(XValue[i], 3);
+                    PowX4[i] = Math.Pow(XValue[i], 4);
+                    PowX2MultY[i] = Math.Pow(XValue[i], 2) * YValue[i];
+                }
+                //находим суммму всех рядов который нам нужны
+                SumXValue = Sum(XValue);
+                SumYValue = Sum(YValue);
+                SumPowX2 = Sum(PowX2);
+                SumPowX3 = Sum(PowX3);
+                SumPowX4 = Sum(PowX4);
+                SumPowX2MultY = Sum(PowX2MultY);
+                SumMultXY = Sum(MultXY);
+
+                Matrix1 = new Matrix(new double[,] {
+                    { SumPowX4, SumPowX3,SumPowX2 },
+                    { SumPowX3,SumPowX2,SumXValue},
+                    { SumPowX2, SumXValue, Count}
+                });
+
+                Matrix2 = new Matrix(new double[,] { { SumPowX2MultY }, { SumMultXY }, { SumYValue } });
+                MatrixInverse = new Matrix(Matrix.FindInverseMatrix(Matrix1.Value));
+                MatrixResult = MatrixInverse * Matrix2;
+
+                A = MatrixResult.Value[0, 0];
+                B = MatrixResult.Value[1, 0];
+                C = MatrixResult.Value[2, 0];
+
+                for (int i = 0; i < Count; ++i)
+                {
+                    YQuadratical[i] = (A * PowX2[i]) + (B * XValue[i]) + C;
+                    D[i] = Y[i] - YQuadratical[i];
+                    PowD2[i] = Math.Pow(D[i], 2);
+                }
+
+                S = Sum(PowD2);
+
+            }
         }
 
 
