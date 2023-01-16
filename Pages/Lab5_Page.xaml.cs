2670f8314a4c10637f06e474f2dffd3144cc9888
nasibullo03 <nasibullo.khalilov@bk.ru>
2023-01-17 00:34:33 +0700

    Создал новый класс SortData для храние значение таблицы, реализовал его основных методов, добавил метод для реализации базовых методов которые выполняются при загрузки приложение,  реализовал метод для добавление данные в таблицу,  реализовал методы для переход в режим заполнение

--- Pages/Lab5_Page.xaml.cs
+++ Pages/Lab5_Page.xaml.cs
@@ -20,16 +20,83 @@ namespace Programing_Labs.Pages
     /// </summary>
     public partial class Lab5_Page : Page
     {
+        #region properties
+        private TextBox TxtBxXi { get; set; }
+        private TextBox TxtBxArraySize { get; set; }
+        private Label LblXi { get; set; }
+        private bool Reverse { get; set; } = false;
+        private List<Control> OnStartControls { get; set; }
+        private List<Control> DataControls_onRandomlyClick { get; set; }
+        private List<Control> DataControls_onManuallyClick { get; set; }
+
+        private List<Task> Tasks = new List<Task>();
+        /// <summary>
+        /// значение i  для Xi
+        /// </summary>
+        public int index = 0;
+
+
+        #endregion
+
         public Lab5_Page()
         {
             InitializeComponent();
+            OlypmSort.SortData.SortDataView = ArrayData_ListView;
+            ArrayData_ListView.ItemsSource = OlypmSort.SortData.SortDataCollection;
         }
 
-        private void BtnFill_Click(object sender, RoutedEventArgs e)
+        private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-
+            AddBaseControlsValues();
         }
+        private void AddBaseControlsValues()
+        {
+            OnStartControls = new List<Control>() { MenuItemArraySize, MenuItemFill };
+            DataControls_onRandomlyClick = new List<Control>() {
+                MenuItemBack,
+                MenuItemAdd,
+                MenuItemCheckBox,
+                MenuItemSortOperation,
+                MenuItemClearOpetations,
 
+            };
+            DataControls_onManuallyClick = new List<Control>() {
+                MenuItemXi,
+                MenuItemBack,
+                MenuItemAdd,
+                MenuItemCheckBox,
+                MenuItemSortOperation,
+                MenuItemClearOpetations,
+
+            };
+
+
+
+            TxtBxXi = GetStyleElement(TextBoxXi, "MainTextBox") as TextBox;
+            TxtBxArraySize = GetStyleElement(TextBoxArraySize, "MainTextBox") as TextBox;
+
+            LblXi = GetStyleElement(LabelXi, "MainLabel") as Label;
+
+
+
+
+            TxtBxXi.PreviewTextInput += new TextCompositionEventHandler(Check.PreviewTextInput);
+            TxtBxArraySize.PreviewTextInput += new TextCompositionEventHandler(Check.PreviewTextInput);
+
+            DataObject.AddPastingHandler(TxtBxXi, (s, a) => a.CancelCommand());
+            DataObject.AddPastingHandler(TxtBxArraySize, (s, a) => a.CancelCommand());
+
+
+            OnStartControls.ForEach(el => el.Visibility = Visibility.Visible);
+            DataControls_onManuallyClick.ForEach(el => el.Visibility = Visibility.Collapsed);
+            MenuItemEdit.Visibility = Visibility.Collapsed;
+        }
+        private object GetStyleElement(Control element, string name) =>
+          element.Template.FindName(name, element);
+
+
+
+        #region Buttons
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
 
@@ -37,14 +104,37 @@ namespace Programing_Labs.Pages
 
         private void BtnBack_Click(object sender, RoutedEventArgs e)
         {
-
+            OnStartControls.ForEach(el => el.Visibility = Visibility.Visible);
+            DataControls_onManuallyClick.ForEach(el => el.Visibility = Visibility.Collapsed);
+            MenuItemBack.Visibility = Visibility.Collapsed;
         }
 
-        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        private async void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
 
-        }
+            double.TryParse(TxtBxXi.Text, out double Xi);
+            double.TryParse(TxtBxArraySize.Text, out double n);
 
+            if (ArrayData_ListView.Items.Count >= n)
+            {
+                MenuItemAdd.Visibility = Visibility.Collapsed;
+                return;
+            }
+            OlypmSort.SortData data = new OlypmSort.SortData(Xi);
+
+            await OlypmSort.SortData.Add(data);
+            if (ArrayData_ListView.Items.Count >= n)
+            {
+                MenuItemAdd.Visibility = Visibility.Collapsed;
+                return;
+            }
+            ++index;
+            LblXi.Content = $"X({index})";
+
+        }
+        #endregion
+
+        #region MenuItems Sort
         private void MenuItem_BubleSort_Click(object sender, RoutedEventArgs e)
         {
 
@@ -69,20 +159,99 @@ namespace Programing_Labs.Pages
         {
 
         }
+        #endregion
 
-        private void BtnRandomGenerate_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        #region Reverse CheckBox
         private void ReverseCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            Reverse = true;
         }
 
         private void ReverseCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
+            Reverse = false;
+        }
+        #endregion
+
+        #region Fill Data
+
+        private void MenuItem_FillManually_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(TxtBxArraySize.Text, out int n);
+
+            if (n <= 0)
+            {
+                MessageBox.Show("Размер массива не может быть меньше или равен нулю!!", "Неверный формат", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            OnStartControls.ForEach(el => el.Visibility = Visibility.Collapsed);
+            DataControls_onManuallyClick.ForEach(el => el.Visibility = Visibility.Visible);
+
+            if (OlypmSort.SortData.EditMode)
+                MenuItemBack.Visibility = Visibility.Visible;
+
+            if (ArrayData_ListView.Items.Count >= n)
+                MenuItemAdd.Visibility = Visibility.Collapsed;
+
+            if (index != 0) return;
+
+            ++index;
+            LblXi.Content = $"X({index})";
+        }
+        private void MenuItem_RandomGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(TxtBxArraySize.Text, out int n);
+            if (n <= 0)
+            {
+                MessageBox.Show("Размер массива не может быть меньше или равен нулю!!", "Неверный формат", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            OnStartControls.ForEach(el => el.Visibility = Visibility.Collapsed);
+            DataControls_onRandomlyClick.ForEach(el => el.Visibility = Visibility.Visible);
+
+            if (OlypmSort.SortData.EditMode)
+                MenuItemBack.Visibility = Visibility.Visible;
+
+            if (ArrayData_ListView.Items.Count >= n)
+                MenuItemAdd.Visibility = Visibility.Collapsed;
+
+            if (index != 0) return;
+        }
+        #endregion
+
+        #region MenuItems Clear 
+
+        private void MenuItemClearAll_Click(object sender, RoutedEventArgs e)
+        {
 
         }
+
+        private void MenuItemClearData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemClearSortData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        #endregion
+
+        #region DataView context items 
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+
     }
 }
---
+++ Pages/OlypmSort/SortData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Programing_Labs.Pages.OlypmSort
+{
+    class SortData
+    {
+        #region properties
+        public static ListView SortDataView { get; set; }
+        /// <summary>
+        /// Коллекция значение для изменение значение
+        /// </summary>
+        public static List<SortData> EditableList { get; set; }
+
+        public static bool EditMode { get; set; }
+        private static int Count { get; set; }
+        public int Index { get; set; }
+        public double Xi { get; set; }
+
+        public static List<SortData> GraphicPoints = new List<SortData>();
+        public static ObservableCollection<SortData> SortDataCollection = new ObservableCollection<SortData>();
+        #endregion
+        #region constructors
+        public SortData()
+        {
+
+        }
+        public SortData(double Xi)
+        {
+            this.Index = ++Count;
+            this.Xi = Xi;
+        }
+        #endregion
+
+        public async static Task Add(SortData sortData)
+        {
+            GraphicPoints.Add(sortData);
+            SortDataCollection.Add(sortData);
+            await Task.Yield();
+        }
+
+        public static void Remove(System.Collections.IList item)
+        {
+            foreach (object el in SortDataView.Items)
+            {
+                if (item.Contains(el))
+                {
+                    GraphicPoints.Remove(el as SortData);
+                }
+            }
+            var sortDataCollection = new ObservableCollection<SortData>();
+            Count = 0;
+            foreach (var el in GraphicPoints.ToArray())
+            {
+                el.Index = ++Count;
+                sortDataCollection.Add(el);
+            }
+
+            SortDataCollection = sortDataCollection;
+            SortDataView.ItemsSource = SortDataCollection;
+            SortDataView.UpdateLayout();
+
+        }
+
+        public static void Clear()
+        {
+            SortDataView.ItemsSource = new ObservableCollection<SortData>();
+            SortDataCollection?.Clear();
+            GraphicPoints?.Clear();
+            Count = 0;
+            SortDataView.ItemsSource = SortDataCollection;
+            EditableList?.Clear();
+        }
+        public static void PrepareDataForEditing(TextBox XiTextBox, Label LblXi)
+        {
+
+            int _index = (EditableList[0] as SortData).Index;
+
+            LblXi.Content = $"X({_index})";
+
+            XiTextBox.Text = (EditableList[0] as SortData).Xi.ToString();
+
+        }
+        public static void EditValues(double Xi)
+        {
+            SortDataCollection[(EditableList[0] as SortData).Index - 1].Xi = Xi;
+            GraphicPoints[(EditableList[0] as SortData).Index - 1].Xi = Xi;
+
+            SortDataView.ItemsSource = new ObservableCollection<SortData>();
+            SortDataView.ItemsSource = SortDataCollection;
+
+
+        }
+        public static void DeleteEditedValue()
+        {
+            if (EditableList.Count != 0)
+                EditableList.RemoveAt(0);
+        }
+
+
+    }
+}
